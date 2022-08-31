039b502efa84bb4850cf8649a97d6b939e55507f
Coral Westoby <coral.westoby@cl.cam.ac.uk>
2022-08-31 14:41:53 +0100

    Add new file to sim dep list.

--- cocotb.mk
+++ cocotb.mk
@@ -1,7 +1,7 @@
 # cocotb defines
 SIM ?= icarus
 TOPLEVEL_LANG ?= verilog
-VERILOG_SOURCES += src/minimal-code-LUT.v
+VERILOG_SOURCES += src/user_module_coralmw_manual_verilog.v
 TOPLEVEL = mkAdder
 MODULE = hardware.simulation.sim
 SIM_BUILD = build
